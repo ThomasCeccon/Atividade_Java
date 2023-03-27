6782617a2a1475b958b53496a7714fd592eb3a4c
Thomas Ceccon <cecconthomas275@gmail.com>
2023-03-27 14:58:52 -0300

    atualizacoes

--- PcMania/Computador.java
+++ PcMania/Computador.java
@@ -10,9 +10,9 @@ public class Computador {
     SistemaOperacional sistemaOperacional;
     MemoriaUSB memoria;
     Cliente cliente; //associacao por agregacao de cliente
-   //Computador[]  c1 = new Computador[100];
+    //Computador[]  c1 = new Computador[100];
 
-     //metodo construtor
+    //metodo construtor
     public Computador(String marca, float preco, String nomeHardware,float clock, int memoriaRam, float capacidade, String nomeSistema, int tipo,MemoriaUSB memousb) {
         this.marca = marca;
         this.preco = preco;
@@ -27,22 +27,28 @@ public class Computador {
         System.out.println("Preco: R$" + this.preco);
         System.out.println("Processador: " + this.hardwareBasico.nomeHardware + " (" + this.hardwareBasico.clock + " Mhz)");
         System.out.println(this.hardwareBasico.memoriaRam + " Gb de Memória RAM");
-        System.out.println(this.hardwareBasico.capacidade + " Gb de HD");
+        if(this.hardwareBasico.capacidade <= 10)
+            System.out.println(this.hardwareBasico.capacidade + " Tb de HD");
+        else
+            System.out.println(this.hardwareBasico.capacidade + " Gb de HD");
         System.out.println(this.sistemaOperacional.nomeSistema + "(" + this.sistemaOperacional.tipo + " bits)");
     }
-   //Computador computador;
+    //Computador computador;
     public void mostraPCConfigs(){
         System.out.println("--------------------------------------------------------------------------------------------------------------------");
         System.out.println("Marca: " + this.marca);
         System.out.println("Preco: R$" + this.preco);
         System.out.println("Processador: " + this.hardwareBasico.nomeHardware + " (" + this.hardwareBasico.clock + " Mhz)");
-        System.out.println(this.hardwareBasico.memoriaRam + " Gb de Memória RAM");
-        System.out.println(this.hardwareBasico.capacidade + " Gb de HD");
+        if(this.hardwareBasico.capacidade <= 10)
+            System.out.println(this.hardwareBasico.capacidade + " Tb de HD");
+        else
+            System.out.println(this.hardwareBasico.capacidade + " Gb de HD");
         System.out.println(this.sistemaOperacional.nomeSistema + "(" + this.sistemaOperacional.tipo + " bits)");
         addMemoriaUSB(memoria);
         System.out.println("--------------------------------------------------------------------------------------------------------------------");
 
     }
+
     public void addMemoriaUSB(MemoriaUSB musb){
         System.out.println("Acompanha "+memoria.nome+ " de "+memoria.capacidade+ "GB");
     }
--- PcMania/Main.java
+++ PcMania/Main.java
@@ -1,5 +1,6 @@
 package PcMania;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -22,15 +23,11 @@ public class Main {
         MemoriaUSB memoriaUSB3=new MemoriaUSB("Pen-Drive",1000);
 
         //colocar valores a cada instancia
+        Computador computador = null;
         Computador comp1 = new Computador("Positivo", 2300, "Pentium core i3", 2200, 8, 500, "Sistema Operacional Linux Ubuntu", 32,memoriaUSB1);
         Computador comp2 = new Computador("Acer", 5800, "Pentium core i5", 3370, 16, 1, "Sistema Operacional Windows 8", 64,memoriaUSB2);
         Computador comp3 = new Computador("Vaio", 1800, "Pentium Core i7", 4500, 32, 2, "Sistema Operacional Windows 10", 64,memoriaUSB3);
-
-        //instancia de cliente
         Cliente cliente=new Cliente();
-
-        cliente.nome="Joao";
-        cliente.cpf=10101001;
 
         //entrada de dados
         System.out.println("--------------------------------------------------------------------------------------------------------------------");
@@ -38,27 +35,34 @@ public class Main {
         Thread.sleep(500);
         System.out.println("Estamos com algumas promoções, para agilizar vou mostrá-las pra você!!");
         Thread.sleep(500);
+        System.out.println("Pode nos informar o seu nome e Cpf? ");
+        cliente.nome = entrada.nextLine();
+        cliente.cpf = entrada.nextLong();
+        System.out.println("Ótimo,cadastro feito!Pode nos falar o quanto no máximo pode gastar?");
+        cliente.carteira = entrada.nextFloat();
+        float carteira_inicial = cliente.carteira;
         System.out.println("--------------------------------------------------------------------------------------------------------------------");
-
+        //mostrando promocao 1
         comp1.mostraPCConfigs1();
         System.out.println("--------------------------------------------------------------------------------------------------------------------");
         Thread.sleep(2000);
-
+        //mostrando promocao 2
         comp2.mostraPCConfigs1();
         System.out.println("--------------------------------------------------------------------------------------------------------------------");
         Thread.sleep(2000);
-
+        //mostrando promocao 3
         comp3.mostraPCConfigs1();
         System.out.println("--------------------------------------------------------------------------------------------------------------------");
         Thread.sleep(2000);
 
-
-        System.out.println("Se interessou por alguma promoção?");
-        System.out.println("1-Promocao 1");
-        System.out.println("2-Promocao 2");
-        System.out.println("3-Promocao 3");
-
         while(flag == true){
+            System.out.println("Se interessou por alguma promoção?");
+            System.out.println("0-Para ir pagar");
+            System.out.println("1-Promocao 1");
+            System.out.println("2-Promocao 2");
+            System.out.println("3-Promocao 3");
+            System.out.println("4-Consultar saldo da carteira");
+            System.out.println("--------------------------------------------------------------------------------------------------------------------");
 
             System.out.print("Digite e aguarde: ");
             opcao = entrada.nextInt();
@@ -73,83 +77,112 @@ public class Main {
             switch (opcao){
                 case 1:
                     comp1.mostraPCConfigs();
-                    System.out.println("Se interessou, gostaria de comprar?");
-                    System.out.println("Digite (1/comprar-2/nao comprar)" );
+                    System.out.println("Se interessou, gostaria de adicionar ao carrinho?");
+                    System.out.println("Digite (1/adicionar-2/nao adicionar)" );
                     int comprar= entrada.nextInt();
-                    if(comprar==1){
-                        cliente.promo1++;
-                        Thread.sleep(500);
-                        System.out.println("PC comprado");
 
+                    if(comprar == 1 && cliente.carteira < comp1.preco){
+                        Thread.sleep(500);
+                        System.out.println("Saldo Insuficiente para comprar esse dispositivo");
+                        System.out.println("Veja outras promoções!");
                     }
-                    else{
+                    if(comprar == 2){
                         Thread.sleep(500);
                         System.out.println("Veja outras promoções!");
                     }
+                    if(comprar==1 && cliente.carteira >= comp1.preco){
+                        cliente.promo1++;
+                        cliente.carteira -= comp1.preco;
+                        Thread.sleep(500);
+                        System.out.println("PC adicionado");
+                    }
+                    System.out.println("--------------------------------------------------------------------------------------------------------------------");
                     break;
 
                 case 2:
                     comp2.mostraPCConfigs();
-                    System.out.println("Digite (1/comprar-2/nao comprar)" );
+                    System.out.println("Digite (1/Adicionar ao carrinho -2/nao adicionar)" );
                     int comprar2= entrada.nextInt();
-                    if(comprar2==1){
-                        cliente.promo2++;
+
+
+                    if(comprar2 == 1 && cliente.carteira < comp2.preco){
                         Thread.sleep(500);
-                        System.out.println("PC comprado");
+                        System.out.println("Saldo Insuficiente para comprar esse dispositivo");
+                        System.out.println("Veja outras promoções!");
                     }
-                    else{
+                    if(comprar2 == 2){
                         Thread.sleep(500);
                         System.out.println("Veja outras promoções!");
                     }
+                    if(comprar2==1&& cliente.carteira >= comp2.preco){
+                        cliente.promo2++;
+                        cliente.carteira -= comp2.preco;
+                        Thread.sleep(500);
+                        System.out.println("PC adicionado");
+                    }
+                    System.out.println("--------------------------------------------------------------------------------------------------------------------");
                     break;
-
 
                 case 3:
+
                     comp3.mostraPCConfigs();
-                    System.out.println("Digite (1/comprar-2/nao comprar)" );
+                    System.out.println("Digite (1/Adicionar ao carrinho-2/nao adicionar)" );
                     int comprar3= entrada.nextInt();
-                    if(comprar3==1){
-                        cliente.promo1++;
+
+                    if(comprar3 == 1 && cliente.carteira < comp3.preco){
                         Thread.sleep(500);
-                        System.out.println("PC comprado");
+                        System.out.println("Saldo Insuficiente para comprar esse dispositivo");
+                        System.out.println("Veja outras promoções!");
                     }
-                    else{
+                    if(comprar3 == 2){
                         Thread.sleep(500);
                         System.out.println("Veja outras promoções!");
                     }
-                    break;
+                    if(comprar3==1&& cliente.carteira >= comp3.preco){
+                        cliente.promo3++;
+                        cliente.carteira -= comp3.preco;
+                        Thread.sleep(500);
+                        System.out.println("PC adicionado");
+                    }
 
+                    System.out.println("--------------------------------------------------------------------------------------------------------------------");
+
+                    break;
+                case 4:
+                    System.out.println("Sua carteira possue saldo de: R$" + cliente.carteira);
+                    System.out.println("--------------------------------------------------------------------------------------------------------------------");
+
+                    break;
                 case 0:
                     flag = false;
-                    System.out.print("Saindo da PC MANIA.");
+                    System.out.print("Indo ao caixa.");
                     Thread.sleep(500);
                     System.out.print(".");
                     Thread.sleep(500);
                     System.out.println(".");
                     Thread.sleep(500);
-
+                    System.out.println("--------------------------------------------------------------------------------------------------------------------");
                     break;
 
                 default:
-                    System.out.println("Só temos 3 promoções, digite novamente!");
+                    System.out.println("Só temos 5 opcoes, digite novamente!");
+                    System.out.println("--------------------------------------------------------------------------------------------------------------------");
+
                     break;
             }
-
-            if(cliente.promo1 > 0)
-            {
-                System.out.println( cliente.nome+" comprou " + cliente.promo1 + " do PC 1");
-            }
-            if(cliente.promo2 > 0 )
-            {
-                System.out.println(cliente.nome +" comprou " + cliente.promo2 + " do PC 2");
-            }
-            if(cliente.promo3 > 0 )
-            {
-                System.out.println( cliente.nome +" comprou " + cliente.promo3 + " do PC 3");
-            }
-
-            float Total_valor = cliente.calculaTotalCompra();
-            System.out.println("Total a pagar de R$ " +Total_valor);
+        }
+        float valor_total = cliente.calculaTotalCompra();
+        System.out.println("Suas compras ficaram: R$" + valor_total);
+        System.out.print("Confirmar compra(true/false): ");
+        boolean confirmar_compra = entrada.nextBoolean();
+        if(confirmar_compra == true){
+            System.out.println("Suas compras ficaram: R$" + valor_total);
+            System.out.println("Seu saldo de carteira ficou: R$" + cliente.carteira);
+        }
+        else
+        {
+            System.out.println("Suas compras ficaram: R$ 0");
+            System.out.println("Seu saldo de carteira ficou: R$" + carteira_inicial);
         }
 
     }
